405cdb341b8dc8cfb1ad1a44e3205c2efbff6934
Luiz Irber <luiz.irber@gmail.com>
2021-12-04 13:34:40 -0800

    add nix configs

---
+++ shell.nix
@@ -0,0 +1,18 @@
+let
+  sources = import ./nix/sources.nix;
+  rustPlatform = import ./nix/rust.nix { inherit sources; };
+  pkgs = import sources.nixpkgs { overlays = [ (import sources.rust-overlay) ]; };
+in
+  with pkgs;
+
+  pkgs.mkShell {
+    buildInputs = [
+      rustPlatform.rust.cargo
+      (python39.withPackages(ps: with ps; [ virtualenv setuptools ]))
+    ];
+
+    shellHook = ''
+       # workaround for https://github.com/NixOS/nixpkgs/blob/48dfc9fa97d762bce28cc8372a2dd3805d14c633/doc/languages-frameworks/python.section.md#python-setuppy-bdist_wheel-cannot-create-whl
+       export SOURCE_DATE_EPOCH=315532800 # 1980
+    '';
+  }
